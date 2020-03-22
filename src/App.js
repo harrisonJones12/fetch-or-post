76fbc19698dfe6831a6279b7655214c224bce4bc
ShavBond <shvranson@gmail.com>
2020-03-21 20:14:24 -0400

    change later

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import {Fetch} from "./components/Fetch";
+// import Main from "./containers/Main";
 
-import Main from "./containers/Main";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      {/* <Main /> */}
+      <Fetch />
     </div>
   );
 }
