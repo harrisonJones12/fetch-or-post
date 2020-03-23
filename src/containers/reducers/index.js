f4681e4f8389d6a526836883bedd8cff34967e36
Harrison Jones <flipguy19@gmail.com>
2020-03-22 20:42:55 -0400

    add initial state / remove console.log statement and api response title and body

--- src/containers/reducers/index.js
+++ src/containers/reducers/index.js
@@ -1,4 +1,8 @@
-const reducer = (state = {}, action) => {
+const initialState = {
+  fetchResponse: {}
+};
+
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "MAKE_POST":
     case "FETCH_POST":
@@ -7,8 +11,8 @@ const reducer = (state = {}, action) => {
       return { ...state, loading: false, post: action.json, showModal: true };
     case "MODAL_CLOSE":
       return { ...state, showModal: false };
-      case "FETCH_SUCCESSFUL":
-      return { ...state, loading: false, fetchResponse: action.jsonFetch  }
+    case "FETCH_SUCCESSFUL":
+      return { ...state, loading: false, fetchResponse: action.jsonFetch };
     default:
       return state;
   }
